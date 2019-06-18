8e0fae09c317f2e6cb0c6cbbcd4efa871f919fdd
ti <ti@ti-dekstop.home>
2019-06-18 23:51:22 +0100

    Add display of methods and entropy

--- DisplayMetrics.py
+++ DisplayMetrics.py
@@ -6,6 +6,7 @@ from Metrics import*
 import pandas as pd
 import matplotlib.pyplot as plt
 import gvxrPython3 as gvxr
+from scipy import stats
 
 def setXRayParameters(SOD, SDD):
     # Compute the source position in 3-D from the SOD
@@ -13,13 +14,14 @@ def setXRayParameters(SOD, SDD):
     gvxr.setDetectorPosition(SOD - SDD, 0.0, 0.0, "cm");
     gvxr.usePointSource();
 
-def display_metrics(Prediction, Target, computingTime):
+def display_metrics(Method, Prediction, Target, computingTime):
 
     number_of_angles = 22;
     number_of_distances = 2;
 
     prediction = Prediction;
     target = Target;
+    method = Method;
     prediction[0] = round(prediction[0]*prediction[1]);
 
     pred_angles = np.zeros(number_of_angles);
@@ -40,6 +42,7 @@ def display_metrics(Prediction, Target, computingTime):
     for i in range(number_of_distances):
         diff.append(abs(prediction[i]-target[i]));
 
+    shannon_entropy = stats.entropy(pred_image);
     SSIM = structural_similarity(pred_image, target_image);
     MAE = mean_absolute_error(target_image, pred_image);
     RMSE = root_mean_squared_error(target_image, pred_image);
@@ -50,18 +53,18 @@ def display_metrics(Prediction, Target, computingTime):
     print('Prediction:' , prediction);
     print('Target: ', target);
     print('SOD and SDD errors: ', diff);
-    print('Metrics: \n SSIM: %.8f \t MAE: %.8f \t RMSE: %.8f \t RE: %.8f \t ZNCC: %.8f'
-            %(SSIM, MAE, RMSE, RE, ZNCC));
+    print('Metrics: \n SSIM: %.8f \t MAE: %.8f \t RMSE: %.8f \t RE: %.8f \t ZNCC: %.8f \
+            \t shannon_entropy' %(SSIM, MAE, RMSE, RE, ZNCC));
 
 
 
-    row = [[prediction[0], diff[0], prediction[1], diff[1], pred_angles,
-            SSIM, MAE, RMSE, RE, ZNCC, computing_time]];
+    row = [[method, prediction[0], diff[0], prediction[1], diff[1], pred_angles,
+            shannon_entropy, SSIM, MAE, RMSE, RE, ZNCC, computing_time]];
 
-    df2 = pd.DataFrame(row, columns=['SOD', 'SOD Error','SDD',
-                                    'SDD Error', 'Rotating Angles','SSIM',
-                                    'MAE', 'RMSE','Relative Error', 'ZNCC',
-                                    'Time']);
+    df2 = pd.DataFrame(row, columns=['Methods', 'SOD', 'SOD Error','SDD',
+                                    'SDD Error', 'Rotating Angles', 'Entropy',
+                                    'SSIM', 'MAE', 'RMSE','Relative Error',
+                                    'ZNCC', 'Time']);
 
 
     return pred_image, df2

f49c9cb3820fd74409c1980891a32316b8c55f11
ti <ti@ti-dekstop.home>
2019-11-22 11:28:28 +0000

    Target now is an image instead of parameters

--- DisplayMetrics.py
+++ DisplayMetrics.py
@@ -13,33 +13,34 @@ def setXRayParameters(SOD, SDD):
     gvxr.setDetectorPosition(SOD - SDD, 0.0, 0.0, "cm");
     gvxr.usePointSource();
 
-def display_metrics(Method, Prediction, Target, computingTime):
+def display_metrics(Method, Prediction, aTargetImage, computingTime):
 
     number_of_angles = 22;
     number_of_distances = 2;
 
     prediction = Prediction;
-    target = Target;
+    # target = Target;
+    target_image = aTargetImage;
     method = Method;
     prediction[0] = round(prediction[0]*prediction[1]);
 
     pred_angles = np.zeros(number_of_angles);
-    target_angles = np.zeros(number_of_angles);
+    # target_angles = np.zeros(number_of_angles);
 
     for i in range(len(pred_angles)):
         pred_angles[i] = prediction[i+number_of_distances];
-        target_angles[i] = target[i+number_of_distances];
+        # target_angles[i] = target[i+number_of_distances];
 
     setXRayParameters(prediction[0], prediction[1]);
     pred_image = bone_rotation(pred_angles);
 
-    setXRayParameters(target[0], target[1]);
-    target_image = bone_rotation(target_angles);
+    # setXRayParameters(target[0], target[1]);
+    # target_image = bone_rotation(target_angles);
 
-    diff = [];
-
-    for i in range(number_of_distances):
-        diff.append(abs(prediction[i]-target[i]));
+    # diff = [];
+    #
+    # for i in range(number_of_distances):
+    #     diff.append(abs(prediction[i]-target[i]));
 
     entropy = shannon_entropy(pred_image);
     SSIM = structural_similarity(pred_image, target_image);
@@ -50,18 +51,18 @@ def display_metrics(Method, Prediction, Target, computingTime):
     computing_time = computingTime;
 
     print('Prediction:' , prediction);
-    print('Target: ', target);
-    print('SOD and SDD errors: ', diff);
+    # print('Target: ', target);
+    # print('SOD and SDD errors: ', diff);
     print('Metrics: \n SSIM: %.8f \t MAE: %.8f \t RMSE: %.8f \t RE: %.8f \t ZNCC: %.8f \
             \t Entropy: %8f' %(SSIM, MAE, RMSE, RE, ZNCC, entropy));
 
 
 
-    row = [[method, prediction[0], diff[0], prediction[1], diff[1], pred_angles,
+    row = [[method, prediction[0], prediction[1], pred_angles,
             entropy, SSIM, MAE, RMSE, RE, ZNCC, computing_time]];
 
-    df2 = pd.DataFrame(row, columns=['Methods', 'SOD', 'SOD Error','SDD',
-                                    'SDD Error', 'Rotating Angles', 'Entropy',
+    df2 = pd.DataFrame(row, columns=['Methods', 'SOD','SDD',
+                                    'Rotating Angles', 'Entropy',
                                     'SSIM', 'MAE', 'RMSE','Relative Error',
                                     'ZNCC', 'Time']);
 

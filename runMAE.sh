7a55e5aa798be6985cda758c6558d91f805094e4
ti <ti@ti-dekstop.home>
2020-04-07 11:58:50 +0100

    add restarting

---
+++ runMAE.sh
@@ -0,0 +1,30 @@
+python3 HandRegistration.py --target "./00382-s1-neg2.png"\
+			    --output "./00382-s1-neg2/MAE-restart"\
+			    --restart 0\
+			    --parameters 24\
+			    --angles 22\
+			    --max_mutation_sigma 0.1\
+   			    --min_mutation_sigma 0.01\
+			    --individuals 20\
+			    --generations 20\
+			    --elitism 0.01\
+			    --new_blood 0.1\
+			    --gaussian_mutation 0.1 0.2\
+			    --blend_cross_over 0.6\
+			    --plot_metrics plot_metrics
+python3 HandRegistration.py --target "./00382-s1-neg2.png"\
+			    --output "./00382-s1-neg2/MAE-restart"\
+			    --restart 1\
+			    --results_csv "./00382-s1-neg2/MAE-restart/results-0.csv"\
+			    --initial_guess initial_guess\
+			    --parameters 24\
+			    --angles 22\
+			    --max_mutation_sigma 0.1\
+   			    --min_mutation_sigma 0.01\
+			    --individuals 1000\
+			    --generations 1000\
+			    --elitism 0.01\
+			    --new_blood 0.1\
+			    --gaussian_mutation 0.1 0.2\
+			    --blend_cross_over 0.6\
+			    --plot_metrics plot_metrics
